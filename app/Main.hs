af338dd33472cc9b161d47d0e39d0706f463d1c0
masaniwa <masaniwasdp@gmail.com>
2017-09-26 05:04:07 +0900

    Modifis metodon de deklari komandon.

--- app/Main.hs
+++ app/Main.hs
@@ -13,39 +13,15 @@ import HakoniwaKilling.Probability (Probability, fromProbability, toProbability)
 
 main :: IO ()
 
-main = run_ $ Group "The calculator of probability that the monster will die, in Hakoniwa Islands."
-    [ subCmd "probability" calcProbability
-    , subCmd "quantity" calcQuantity
-    , subCmd "transition" calcTransition ]
-
-
--- Tipo de HP argumento.
-type FlagHP = Flag "h" '["hp"] "Integer" "The HP of the monster." Integer
-
--- Tipo de kvanto argumento.
-type FlagQuantity = Flag "q" '["quantity"] "Integer" "The quantity of missiles to launch." Integer
-
--- Tipo de probablo argumento.
-type FlagProbability = Flag "p" '["probability"] "Double(%)" "The probability of killing the monster." Double
-
--- Tipo de minimuma kvanto argumento.
-type FlagMin = Flag "n" '["min"] "Integer" "The minimum quantity of missiles to launch." Integer
-
--- Tipo de maksimuma kvanto argumento.
-type FlagMax = Flag "m" '["max"] "Integer" "The maximum quantity of missiles to launch." Integer
-
--- Tipo de probablo komando.
-type CmdProbability = Cmd "Calculates probability that the monster will die." ()
-
--- Tipo de kvanto komando.
-type CmdQuantity = Cmd "Calculates enough quantity of missiles to kill the monster." ()
-
--- Tipo de transiro komando.
-type CmdTransition = Cmd "Calculates probability transition that the monster dies when launching m missiles from n." ()
+main = run_ $ Group
+    "The calculator of probability that the monster will die, in Hakoniwa Islands."
+    [subCmd "probability" calcProbability , subCmd "quantity" calcQuantity , subCmd "transition" calcTransition]
 
 
 -- Kalkulas probablon de sukcesi mortigi monstron.
-calcProbability :: FlagHP -> FlagQuantity -> CmdProbability
+calcProbability :: Flag "h" '["hp"] "Num" "The HP of the monster." Integer
+                -> Flag "q" '["quantity"] "Num" "The quantity of missiles to launch." Integer
+                -> Cmd "Calculates probability that the monster will die." ()
 
 calcProbability hp quantity = putResult result "Failed to calculate the probability." where
     result      = showPercentage <$> probability
@@ -53,7 +29,9 @@ calcProbability hp quantity = putResult result "Failed to calculate the probabil
 
 
 -- Kalkulas postulitan kvanton da misiloj por mortigi monstron.
-calcQuantity :: FlagHP -> FlagProbability -> CmdQuantity
+calcQuantity :: Flag "h" '["hp"] "Num" "The HP of the monster." Integer
+             -> Flag "p" '["probability"] "Num(%)" "The probability of killing the monster." Double
+             -> Cmd "Calculates enough quantity of missiles to kill the monster." ()
 
 calcQuantity hp probability = putResult result "Failed to calculate the quantity." where
     result = case quantity of
@@ -65,7 +43,10 @@ calcQuantity hp probability = putResult result "Failed to calculate the quantity
 
 
 -- Kalkulas transiron de probablo de sukcesi mortigi monstron.
-calcTransition :: FlagHP -> FlagMin -> FlagMax -> CmdTransition
+calcTransition :: Flag "h" '["hp"] "Num" "The HP of the monster." Integer
+               -> Flag "n" '["min"] "Num" "The minimum quantity of missiles to launch." Integer
+               -> Flag "m" '["max"] "Num" "The maximum quantity of missiles to launch." Integer
+               -> Cmd "Calculates probability transition that the monster dies when launching m missiles from n." ()
 
 calcTransition hp n m = putResult result "Failed to calculate the transition." where
     result     = intercalate "\n" . map showPercentage <$> transition
@@ -76,14 +57,16 @@ calcTransition hp n m = putResult result "Failed to calculate the transition." w
 
 
 -- Konvertas probablon al kordo.
-showPercentage :: Probability -> String
+showPercentage :: Probability
+               -> String
 
 showPercentage p = (++ "%") . printf "%.3f" $ percentage where
     percentage = (* 100) . fromRational . fromProbability $ p :: Double
 
 
 -- Akiras naturan nombron de argumento.
-getNatural :: Flag a b c d Integer -> Maybe Natural
+getNatural :: Flag a b c d Integer
+           -> Maybe Natural
 
 getNatural x
     | get x < 0 = Nothing
@@ -91,6 +74,8 @@ getNatural x
 
 
 -- Montras kalkulon rezulton.
-putResult :: Maybe String -> String -> Cmd a ()
+putResult :: Maybe String
+          -> String
+          -> Cmd a ()
 
 putResult result instead = liftIO . putStrLn $ fromMaybe instead result
