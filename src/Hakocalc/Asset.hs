36e7540a54ccdc8ec6a4fc822be9602ad5608d8a
masaniwa <masaniwasdp@gmail.com>
2018-11-11 08:58:54 +0900

    Modifis stilon.

--- src/Hakocalc/Asset.hs
+++ src/Hakocalc/Asset.hs
@@ -44,33 +44,29 @@ failed = "Couldn't calculate."
 {-| -}
 desc :: DescKey -> String
 
-desc x = case x of
-  DescProg -> "Calculates probability that a monster will die, in the Hakoniwa Islands."
-  DescCmdP -> "Calculates probability that a monster will die."
-  DescCmdQ -> "Calculates quantity of missiles to kill a monster."
+desc DescProg = "Calculates probability that a monster will die, in the Hakoniwa Islands."
+desc DescCmdP = "Calculates probability that a monster will die."
+desc DescCmdQ = "Calculates quantity of missiles to kill a monster."
 
 
 {-| -}
 command :: CmdKey -> String
 
-command x = case x of
-  CmdP -> "probability"
-  CmdQ -> "quantity"
+command CmdP = "probability"
+command CmdQ = "quantity"
 
 
 {-| -}
 metavar :: OptKey -> String
 
-metavar x = case x of
-  OptH -> "HP"
-  OptP -> "PROBABILITY"
-  OptQ -> "MISSILES"
+metavar OptH = "HP"
+metavar OptP = "PROBABILITY"
+metavar OptQ = "MISSILES"
 
 
 {-| -}
 help :: OptKey -> String
 
-help x = case x of
-  OptH -> "The HP of the monster."
-  OptP -> "The probability of killing the monster. (%)"
-  OptQ -> "The quantity of missiles to launch."
+help OptH = "The HP of the monster."
+help OptP = "The probability of killing the monster. (%)"
+help OptQ = "The quantity of missiles to launch."
