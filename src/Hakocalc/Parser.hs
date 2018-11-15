2b2ccae0d949e62a7b3e65aec01671f1081e99e0
masaniwa <masaniwasdp@gmail.com>
2018-11-16 01:24:47 +0900

    Modifis stilon.

--- src/Hakocalc/Parser.hs
+++ src/Hakocalc/Parser.hs
@@ -27,44 +27,40 @@ data Option = POption PArgs | QOption QArgs
 {-| Analizas komandolinion opcion. -}
 optionParser :: ParserInfo Option
 
-optionParser = info pars desc
+optionParser = info pars $ desc Asset.DescProg
   where
     pars = subparser $ pOptionParser <> qOptionParser
-
-    desc = progDesc $ Asset.desc Asset.DescProg
 
 
 {-| Analizas opcion por probablo komando. -}
 pOptionParser :: Mod CommandFields Option
 
-pOptionParser = command (Asset.command Asset.CmdP) (info pars desc)
+pOptionParser = command (Asset.command Asset.CmdP) (info pars $ desc Asset.DescCmdP)
   where
     pars = helper <*> fmap POption args
 
     args = PArgs <$> helpArg Asset.OptH <*> helpArg Asset.OptQ
 
-    desc = progDesc $ Asset.desc Asset.DescCmdP
-
 
 {-| Analizas opcion por kvanto komando. -}
 qOptionParser :: Mod CommandFields Option
 
-qOptionParser = command (Asset.command Asset.CmdQ) (info pars desc)
+qOptionParser = command (Asset.command Asset.CmdQ) (info pars $ desc Asset.DescCmdQ)
   where
     pars = helper <*> fmap QOption args
 
     args = QArgs <$> helpArg Asset.OptH <*> helpArg Asset.OptP
 
-    desc = progDesc $ Asset.desc Asset.DescCmdQ
-
 
 {-| Kreas argumenton analizilon kun helpo teksto. -}
-helpArg :: Read a
-  => Asset.OptKey -- ^ Ŝlosilo de la opcio.
-  -> Parser a     -- ^ Argumento analizilo.
+helpArg :: Read a => Asset.OptKey -> Parser a
 
-helpArg k = argument auto $ h <> m
+helpArg x = argument auto $ h <> m
   where
-    h = help $ Asset.help k
+    h = help $ Asset.help x
 
-    m = metavar $ Asset.metavar k
+    m = metavar $ Asset.metavar x
+
+
+{-| -}
+desc = progDesc . Asset.desc
