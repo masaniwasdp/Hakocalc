f8e2ccf832dd12f95825742c11801ecbfdf9b0cf
masaniwa <masaniwasdp@gmail.com>
2019-01-27 02:05:08 +0900

    Modifis efektivigon.

--- src/Hakocalc/Options.hs
+++ src/Hakocalc/Options.hs
@@ -10,9 +10,9 @@ module Hakocalc.Options
 
 import Data.Semigroup ((<>))
 import Hakocalc.Command (Option (POpts, QOpts))
-import Hakocalc.OptUtil (optionP, optionQ)
-import Options.Applicative.Builder (command, info, subparser, progDesc)
-import Options.Applicative.Common (ParserInfo)
+import Options.Applicative (command, helper, info, progDesc, subparser)
+import Options.Applicative.Builder (argument, auto, help, metavar, subparser)
+import Options.Applicative.Types (Parser, ParserInfo)
 
 import qualified Hakocalc.Asset.Text as Text
 
@@ -24,3 +24,21 @@ options = info (subparser $ p <> q) (progDesc Text.descA)
   where
     p = command Text.commandP . info optionP $ progDesc Text.descP
     q = command Text.commandQ . info optionQ $ progDesc Text.descQ
+
+
+{-| -}
+optionP :: Parser Option
+
+optionP = helper <*> (POpts <$> h <*> q)
+  where
+    h = argument auto $ help Text.helpH <> metavar Text.metavarH
+    q = argument auto $ help Text.helpQ <> metavar Text.metavarQ
+
+
+{-| -}
+optionQ :: Parser Option
+
+optionQ = helper <*> (QOpts <$> h <*> p)
+  where
+    h = argument auto $ help Text.helpH <> metavar Text.metavarH
+    p = argument auto $ help Text.helpP <> metavar Text.metavarP
