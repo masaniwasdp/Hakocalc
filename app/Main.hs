46353237018386d29312bc36e85836ccf7d38a97
masaniwa <masaniwasdp@gmail.com>
2019-02-18 03:29:07 +0900

    Modifis la strukturon.

--- app/Main.hs
+++ app/Main.hs
@@ -5,7 +5,4 @@ import Options.Applicative (customExecParser, prefs, showHelpOnEmpty)
 
 main :: IO ()
 
-main = do
-  opt <- customExecParser (prefs showHelpOnEmpty) options
-
-  putStrLn $ command opt
+main = command =<< customExecParser (prefs showHelpOnEmpty) options
--- src/Hakocalc/App/Command.hs
+++ src/Hakocalc/App/Command.hs
@@ -1,11 +1,10 @@
 {-|
- - Description : Modulo de komando.
+ - Description : Modulo por ekzekuti komandoj.
  - Copyright   : 2019 masaniwa
  - License     : MIT
  -}
 module Hakocalc.App.Command
   ( Option (..)
-  , Result
   , command
   ) where
 
@@ -17,20 +16,22 @@ import Text.Printf (printf)
 import qualified Hakocalc.Asset.Text as Text
 
 
-{-| Rezolto de komandoj. -}
-type Result = String
-
-
 {-| -}
 data Option = OptionP HP Quantity | OptionQ HP Probability
 
 
 {-| -}
-command :: Option -> Result
+command :: Option -> IO ()
 
-command (OptionP h q) = toPercent $ defeatProbability h q
+command = putStrLn . calculate
 
-command (OptionQ h p) = maybe Text.failed show $ enoughMissiles h p
+
+{-| -}
+calculate :: Option -> String
+
+calculate (OptionP h q) = toPercent $ defeatProbability h q
+
+calculate (OptionQ h p) = maybe Text.failed show $ enoughMissiles h p
 
 
 {-| Konvertas probablon al procento kordo. -}
