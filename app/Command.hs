49e57383f92f30a4e87dfb3b8903e72cba5454f3
masaniwa <masaniwasdp@gmail.com>
2019-05-05 01:36:03 +0900

    Aldonis Command al app.

---
+++ app/Command.hs
@@ -0,0 +1,28 @@
+module Command where
+
+
+import Hakocalc.Entity.Defeat (HP, Probability, Quantity, missiles, probability)
+import Params (Params (ParamsP, ParamsQ))
+import Text.Printf (printf)
+
+
+command :: Params -> IO ()
+
+command (ParamsP h q) = responseProbability $ probability h q
+
+command (ParamsQ h p) = maybe notifyFailed responseQuantity $ missiles h p
+
+
+responseQuantity :: Quantity -> IO ()
+
+responseQuantity = printf "%d\n"
+
+
+responseProbability :: Probability :: IO ()
+
+responseProbability = (printf "%.3f%%\n") . (read :: String -> Double) . show
+
+
+notifyFailed :: IO ()
+
+notifyFailed = putStrLn "Could not calculate."
