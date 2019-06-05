19d36dd07c43e3533a824a9b908362050af903c8
masaniwa <masaniwasdp@gmail.com>
2019-06-06 03:44:11 +0900

    Modifis stilon.

---
+++ src/Hakocalc/Command/Helper.hs
@@ -0,0 +1,19 @@
+module Hakocalc.Command.Helper
+  ( defineArg
+  , defineCmd
+  ) where
+
+
+import Data.Semigroup ((<>))
+import Options.Applicative (CommandFields, Mod, Parser, command, helper, info, progDesc)
+import Options.Applicative.Builder (argument, auto, help, metavar)
+
+
+defineCmd :: Parser a -> String -> String -> Mod CommandFields a
+
+defineCmd p n d = command n . info (helper <*> p) $ progDesc d
+
+
+defineArg :: Read a => String -> String -> Parser a
+
+defineArg h m = argument auto $ help h <> metavar m
--- src/Hakocalc/Command/Main.hs
+++ src/Hakocalc/Command/Main.hs
@@ -5,20 +5,21 @@ module Hakocalc.Command.Main
 
 import Control.Lens ((^.))
 import Data.Semigroup ((<>))
+import Hakocalc.Command.Helper (defineArg, defineCmd)
+import Options.Applicative (CommandFields, Mod, ParserInfo, info, progDesc, subparser)
 
 import qualified Hakocalc.Command.Behavior as B
 import qualified Hakocalc.Command.Config as C
-import qualified Options.Applicative as A
 
 
-command :: C.Config -> A.ParserInfo (IO ())
+command :: C.Config -> ParserInfo (IO ())
 
-command cfg = A.info p $ A.progDesc (cfg ^. C.descA)
+command cfg = info p $ progDesc (cfg ^. C.descA)
   where
-    p = A.subparser $ (commandP cfg) <> (commandQ cfg)
+    p = subparser $ (commandP cfg) <> (commandQ cfg)
 
 
-commandP :: C.Config -> A.Mod A.CommandFields (IO ())
+commandP :: C.Config -> Mod CommandFields (IO ())
 
 commandP cfg = defineCmd p (cfg ^. C.nameP) (cfg ^. C.descP)
   where
@@ -27,20 +28,10 @@ commandP cfg = defineCmd p (cfg ^. C.nameP) (cfg ^. C.descP)
       <*> (defineArg (cfg ^. C.helpQ) (cfg ^. C.metaQ))
 
 
-commandQ :: C.Config -> A.Mod A.CommandFields (IO ())
+commandQ :: C.Config -> Mod CommandFields (IO ())
 
 commandQ cfg = defineCmd p (cfg ^. C.nameQ) (cfg ^. C.descQ)
   where
     p = (B.commandQ cfg)
       <$> (defineArg (cfg ^. C.helpH) (cfg ^. C.metaH))
       <*> (defineArg (cfg ^. C.helpP) (cfg ^. C.metaP))
-
-
-defineCmd :: A.Parser a -> String -> String -> A.Mod A.CommandFields a
-
-defineCmd p n d = A.command n . A.info (A.helper <*> p) $ A.progDesc d
-
-
-defineArg :: Read a => String -> String -> A.Parser a
-
-defineArg h m = A.argument A.auto $ A.help h <> A.metavar m
